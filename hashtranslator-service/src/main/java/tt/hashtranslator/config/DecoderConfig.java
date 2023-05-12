bc97e2d3067fe0322c29c2d902e0ff1675098764
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-05-12 16:02:34 +0300

    Add: fault tolerant

--- hashtranslator-service/src/main/java/tt/hashtranslator/config/DecoderConfig.java
+++ hashtranslator-service/src/main/java/tt/hashtranslator/config/DecoderConfig.java
@@ -1,7 +1,5 @@
 package tt.hashtranslator.config;
 
-import static io.netty.channel.ChannelOption.CONNECT_TIMEOUT_MILLIS;
-
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +10,12 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import io.github.resilience4j.common.circuitbreaker.configuration.CircuitBreakerConfigCustomizer;
+
+import java.time.Duration;
+
+import static io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType.COUNT_BASED;
+import static io.netty.channel.ChannelOption.CONNECT_TIMEOUT_MILLIS;
 
 @Configuration
 public class DecoderConfig {
@@ -24,15 +28,19 @@ public class DecoderConfig {
 
   private final int connectTimeout;
 
+  private final int minNumberOfCalls;
+
   public DecoderConfig(
       @Value("${decoder.url}") final String md5DecoderUri,
       @Value("${decoder.read.timeout.sec}") final int readTimeout,
       @Value("${decoder.write.timeout.sec}") final int writeTimeout,
-      @Value("${decoder.connect.timeout.ms}") final int connectTimeout) {
+      @Value("${decoder.connect.timeout.ms}") final int connectTimeout,
+      @Value("${decoder.min.calls}") final int minNumberOfCalls) {
     this.md5DecoderUri = md5DecoderUri;
     this.readTimeout = readTimeout;
     this.writeTimeout = writeTimeout;
     this.connectTimeout = connectTimeout;
+    this.minNumberOfCalls = minNumberOfCalls;
   }
 
   @Bean
@@ -52,5 +60,18 @@ public class DecoderConfig {
                         .addHandlerLast(new WriteTimeoutHandler(writeTimeout)));
     final ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
     return WebClient.builder().baseUrl(md5DecoderUri).clientConnector(connector).build();
+  }
+
+  @Bean
+  public CircuitBreakerConfigCustomizer externalServiceFooCircuitBreakerConfig() {
+    return CircuitBreakerConfigCustomizer.of(
+        "decoderService",
+        builder ->
+            builder
+                .slidingWindowSize(10)
+                .slidingWindowType(COUNT_BASED)
+                .waitDurationInOpenState(Duration.ofSeconds(5))
+                .minimumNumberOfCalls(minNumberOfCalls)
+                .failureRateThreshold(50.0f));
   }
 }
--- hashtranslator-service/src/main/java/tt/hashtranslator/service/DecoderServiceImpl.java
+++ hashtranslator-service/src/main/java/tt/hashtranslator/service/DecoderServiceImpl.java
@@ -1,10 +1,8 @@
 package tt.hashtranslator.service;
 
-import static java.util.stream.Collectors.toList;
-import static org.springframework.http.HttpStatus.OK;
-import static reactor.core.publisher.Mono.just;
-
-import java.util.List;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
@@ -15,6 +13,12 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import tt.hashtranslator.domain.entity.Application;
 import tt.hashtranslator.repository.ApplicationRepository;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.HttpStatus.OK;
+import static reactor.core.publisher.Mono.just;
 
 @Slf4j
 @Service
@@ -28,40 +32,54 @@ public class DecoderServiceImpl implements DecoderService {
   private final String hasPath;
   private final ApplicationRepository applicationRepository;
   private final WebClient hashDecoderClient;
+  private final CircuitBreakerRegistry circuitBreakerRegistry;
 
   public DecoderServiceImpl(
       @Value("${hash.type}") final String hashType,
       @Value("${hash.path}") final String hasPath,
       final ApplicationRepository applicationRepository,
-      final WebClient hashDecoderClient) {
+      final WebClient hashDecoderClient,
+      final CircuitBreakerRegistry circuitBreakerRegistry) {
     this.hashType = hashType;
     this.hasPath = hasPath;
     this.applicationRepository = applicationRepository;
     this.hashDecoderClient = hashDecoderClient;
+    this.circuitBreakerRegistry = circuitBreakerRegistry;
   }
 
   @Async
   @Override
   public void decode(final Application application) {
-    final List<String> request = application.getHashes().stream().map(String::toLowerCase)
-        .collect(toList());
+    final List<String> request =
+        application.getHashes().stream().map(String::toLowerCase).collect(toList());
     final Flux<String> resultHashes = Flux.fromIterable(request).flatMap(this::sendHashesToDecoder);
     final Mono<List<String>> failed = resultHashes.filter(request::contains).collectList();
-    final Mono<List<String>> success = resultHashes.filter(hash -> !request.contains(hash))
-        .collectList();
-    success.publishOn(Schedulers.boundedElastic()).map(successHash -> {
-      application.setSuccessResult(successHash);
-      return applicationRepository.save(application);
-    }).subscribe(
-        app -> log.info("Finished processing with Success Hashes = {}", app.getSuccessResult()));
-    failed.publishOn(Schedulers.boundedElastic()).map(failedHash -> {
-      application.setFailedResult(failedHash);
-      return applicationRepository.save(application);
-    }).subscribe(
-        app -> log.info("Finished processing with Failed Hashes = {}", app.getFailedResult()));
+    final Mono<List<String>> success =
+        resultHashes.filter(hash -> !request.contains(hash)).collectList();
+    success
+        .publishOn(Schedulers.boundedElastic())
+        .map(
+            successHash -> {
+              application.setSuccessResult(successHash);
+              return applicationRepository.save(application);
+            })
+        .subscribe(
+            app ->
+                log.info("Finished processing with Success Hashes = {}", app.getSuccessResult()));
+    failed
+        .publishOn(Schedulers.boundedElastic())
+        .map(
+            failedHash -> {
+              application.setFailedResult(failedHash);
+              return applicationRepository.save(application);
+            })
+        .subscribe(
+            app -> log.info("Finished processing with Failed Hashes = {}", app.getFailedResult()));
   }
 
   private Mono<String> sendHashesToDecoder(final String hash) {
+    final CircuitBreaker circuitBreaker =
+        circuitBreakerRegistry.circuitBreaker("decoderService", "decoderService");
     return hashDecoderClient
         .get()
         .uri(
@@ -75,6 +93,7 @@ public class DecoderServiceImpl implements DecoderService {
             response ->
                 response.statusCode().equals(OK)
                     ? response.bodyToMono(String.class).defaultIfEmpty(hash)
-                    : just(EMPTY_STRING));
+                    : just(EMPTY_STRING))
+        .transformDeferred(CircuitBreakerOperator.of(circuitBreaker));
   }
 }
